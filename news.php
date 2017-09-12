be26a3489ba8a5875dfd01fee3c72bfd0ed3245d
xuya123 <31750749+xuya123@users.noreply.github.com>
2017-09-12 18:18:07 +0800

    新闻中心
    新闻中心

---
+++ news.php
@@ -0,0 +1,36 @@
+﻿<?php 
+	//<!-- 头部 -->
+	require 'news_header.html';
+	
+	//<!-- 导航 -->
+	require 'news_nav.html';
+
+	//<!-- banner -->	
+	require 'news_banner.html';
+	require 'news_mysql.php';
+?>	
+
+	<!-- 中部开始 -->
+		<?php require 'news_type.html'; ?>
+		
+		<?php require 'news_content.html'; ?>
+		<div class="page">
+			<ol class="clearfix">
+				<li><a class="prev" href="#">上一页</a></li>
+				<li><a class="on" href="#">1</a></li>
+				<li><a href="#">2</a></li>
+				<li><a href="#">3</a></li>
+				<li><a class="next" href="#">下一页</a></li>
+			</ol>
+		</div>
+	</div>
+	<!-- 中部结束 -->
+
+
+	<!-- 底部 -->
+	<?php 
+		require 'news_footer.html';
+	?>
+	
+</body>
+</html>
---
+++ news_mysql.php
@@ -0,0 +1,23 @@
+﻿<?php
+	
+	require '../confing.php';
+	require '../mysqli.php';
+
+	
+	//获取新闻类型
+	$sql="select * from new_types";
+	
+	$link=conn($confing);
+	// print_r($link);
+	$result=query($link,$sql);
+	//print_r($result);
+	$new_types=fetch_assoc($result);
+	//var_dump($new_types);
+	
+	//获取新闻
+	$sql="select * from news";
+	
+	$link=conn($confing);
+	$result=query($link,$sql);
+	
+	$news=fetch_assoc($result);
